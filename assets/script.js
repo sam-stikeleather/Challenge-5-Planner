491cc0ce5cc97d8792f57c122a7911219c2b8563
Sam <sam.stikeleather@gmail.com>
2023-07-08 14:46:48 -0400

    began JS

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,34 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
 
-// import dayjs from 'dayjs';
+$(document).ready(function() {
+// Get the current date and format it 
+  const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+  $('#currentDay').text(currentDate);
+  
+//Add event listeners to the save buttons
+$('.saveBtn').on('click', function() {
+  const timeBlock = $(this).parent();
+  const time = timeBlock.attr('id');
+  const eventText = timeBlock.find('description').val();
+
+// Save the event text to local storage
+localStorage.setItem(time, eventText);  
+});
+
+// Load and saved events from local storage
+function loadEvents() {
+  $('.time-block').each(function() {
+    const time = $(this).attr.getItem(time);
+
+    if (eventText !== null) {
+      $(thus).find('.description').val(eventText);
+    }
+  });
+}
+
+
+})
+
+
 
 let now = dayjs();
 
