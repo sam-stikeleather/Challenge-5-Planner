ae56d5fa9061f7bd3ed83a0b7ce523a6b66cceef
Sam <sam.stikeleather@gmail.com>
2023-07-08 15:00:36 -0400

    bug fixes

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,54 @@
-
 $(document).ready(function() {
-// Get the current date and format it 
-  const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+  // Get the current date and format it
+  var currentDate = dayjs().format('dddd, MMMM D, YYYY');
   $('#currentDay').text(currentDate);
-  
-//Add event listeners to the save buttons
-$('.saveBtn').on('click', function() {
-  const timeBlock = $(this).parent();
-  const time = timeBlock.attr('id');
-  const eventText = timeBlock.find('description').val();
 
-// Save the event text to local storage
-localStorage.setItem(time, eventText);  
+  // Add event listeners to the save buttons
+  $('.saveBtn').on('click', function() {
+    var timeBlock = $(this).parent();
+    var time = timeBlock.attr('id');
+    var eventText = timeBlock.find('.description').val();
+    
+    // Save the event text to local storage
+    localStorage.setItem(time, eventText);
+  });
+
+  // Load any saved events from local storage
+  function loadEvents() {
+    $('.time-block').each(function() {
+      var time = $(this).attr('id');
+      var eventText = localStorage.getItem(time);
+
+      if (eventText !== null) {
+        $(this).find('.description').val(eventText);
+      }
+    });
+  }
+
+  loadEvents();
+
+  // Update the color coding of time blocks
+  function updateTimeBlocks() {
+    var currentHour = dayjs().hour();
+
+    $('.time-block').each(function() {
+      var blockHour = parseInt($(this).attr('id').split('-')[1]);
+
+      $(this).removeClass('past present future');
+
+      if (blockHour < currentHour) {
+        $(this).addClass('past');
+      } else if (blockHour === currentHour) {
+        $(this).addClass('present');
+      } else {
+        $(this).addClass('future');
+      }
+    });
+  }
+
+  updateTimeBlocks();
 });
 
-// Load and saved events from local storage
-function loadEvents() {
-  $('.time-block').each(function() {
-    const time = $(this).attr.getItem(time);
-
-    if (eventText !== null) {
-      $(thus).find('.description').val(eventText);
-    }
-  });
-}
-
-
-})
 
 
 
